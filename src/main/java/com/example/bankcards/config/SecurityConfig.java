d872e4f22bb003f608daacae630120fdcd8c8ab9
Zzzrbn <zzzrbn@yandex.ru>
2025-10-15 13:18:34 +0300

    Remove log directory and add to gitignore

--- src/main/java/com/example/bankcards/config/SecurityConfig.java
+++ src/main/java/com/example/bankcards/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.bankcards.config;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -59,16 +60,11 @@ public class SecurityConfig {
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
+	    
 		CorsConfiguration configuration = new CorsConfiguration();
-
-		configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000", "http://localhost:4200",
-				"http://localhost:8081", "https://bank_rest-main.com"));
-
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept",
-				"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-
+		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
 
 		configuration.setMaxAge(3600L);
